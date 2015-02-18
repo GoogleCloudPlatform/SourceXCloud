aa5dc206949dbfc5abd9abf15e56d0e630a09271
Michael Muller <mmuller@google.com>
2015-02-18 10:58:30 -0500

    Manage subprocess output in real-time.
    Move subprocess management into crepuscular.proclib and use a select loop,
    allowing us to interact with child process output in real-time.  This lets us
    see the output of tools like gcloud in real-time.  Also make it so that a
    hook can output multiple json objects, one per line on standard output, to be
    sent back to the framework.

---
+++ lib/crepuscular/proclib.py
@@ -0,0 +1,99 @@
+"""Utilities for dealing with child processes."""
+
+import fcntl
+import os
+import subprocess
+import select
+
+
+class _LineAccumulator(object):
+    """Accumulates data and calls a callback for every line."""
+
+    def __init__(self, lineCallback):
+        self.__buffer = []
+        self.__cb = lineCallback
+
+    def add(self, data):
+        # If there is a newline, call the callback on the data we've got so
+        # far plus the newline.
+        newline_pos = data.find('\n') + 1
+        while newline_pos > 0:
+            line = ''.join(self.__buffer) + data[:newline_pos]
+            self.__buffer = []
+            self.__cb(line)
+            data = data[newline_pos:]
+            newline_pos = data.find('\n') + 1
+
+        # add whatever's leftover to the buffer.
+        if data:
+            self.__buffer.append(data)
+
+    def finish(self):
+        self.__cb(''.join(self.__buffer))
+        self.__buffer = []
+
+
+def _set_nonblocking(f):
+    fd = f.fileno()
+    flags = fcntl.fcntl(fd, fcntl.F_GETFL)
+    fcntl.fcntl(fd, fcntl.F_SETFL, flags | os.O_NONBLOCK)
+
+def run(*args, **kwargs):
+    stdout_callback = kwargs.get('stdout_callback')
+    stderr_callback = kwargs.get('stderr_callback')
+    pipe_error = kwargs.get('pipe_error_callback')
+    stdin = kwargs.get('stdin')
+
+    # Define the accumulators to help us manage the process output.
+    stdout_accumulator = _LineAccumulator(stdout_callback)
+    stderr_accumulator = _LineAccumulator(stderr_callback)
+
+    proc = subprocess.Popen(args,
+                            stdout=subprocess.PIPE if stdout_callback else None,
+                            stderr=subprocess.PIPE if stderr_callback else
+                            None,
+                            stdin=subprocess.PIPE if stdin else None)
+    inputs = [proc.stdout, proc.stderr]
+    outputs = [proc.stdin] if stdin else []
+
+    # Make the inputs non-blocking
+    _set_nonblocking(proc.stdout)
+
+    while inputs or outputs:
+        rdx, wrx, erx = select.select(inputs, outputs, inputs + outputs)
+
+        # Remove any error pipes from consideration.
+        for p in erx:
+            if pipe_error:
+                if p is proc.stderr:
+                    name = 'stderr'
+                elif p is proc.stdout:
+                    name = 'stdout'
+                elif p is proc.stdin:
+                    name = 'stdin'
+                pipe_error(name)
+            inputs.remove(p)
+
+        # Feed in the next chunk of standard input.
+        if wrx:
+            if stdin:
+                wrx[0].write(stdin[:1024])
+                wrx[0].flush()
+                stdin = stdin[1024:]
+            else:
+                wrx[0].close()
+                outputs = []
+
+        # Read everything from the process.
+        for p in rdx:
+            data = os.read(p.fileno(), 1024)
+            if p is proc.stdout:
+                stdout_accumulator.add(data)
+            else:
+                stderr_accumulator.add(data)
+            if not data:
+                inputs.remove(p)
+
+    stdout_accumulator.finish()
+    stderr_accumulator.finish()
+    return proc.wait()
